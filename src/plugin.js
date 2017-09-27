810d0ca09aedaf6d47c2560864ef26d9fdd81035
Aram Baghdasaryan <xquack@gmail.com>
2017-09-27 22:05:35 +0400

    Plugin updated to support options

--- src/plugin.js
+++ src/plugin.js
@@ -1,21 +1,37 @@
-Selectize.define('delayed_select_on_enter', function (options) {
+Selectize.define('delayed_select', function (options) {
 	var self = this;
+	var defaults = {
+		filter: false,
+		selectSingleMatch: true
+	};
+
+	self.__ds = {
+		enterPressed: false,
+		options: $.extend({}, defaults, options || {})
+	};
+
 
 	this.load = (function(fn) {
 		var original = self.load;
 
-		self.enterPressed = false;
+		self.__ds.enterPressed = false;
 
 		self.on('load', function (result) {
-			if (!self.enterPressed) return;
+			if (!self.__ds.enterPressed) return;
+
+			if (self.__ds.options.selectSingleMatch && typeof result !== 'undefined' && result.length == 1) {
+				self.addItem(result.shift().id);
+				return;
+			}
 
 			var code = self.$control_input.val();
 
 			for (var i in result) {
 				if (!result.hasOwnProperty(i)) continue;
 
-				if (result[i].code == code) {
+				if (self.__ds.options.filter(code, result[i])) {
 					self.addItem(result[i].id);
+					return;
 				}
 			}
 		});
@@ -30,7 +46,7 @@ Selectize.define('delayed_select_on_enter', function (options) {
 
 		return function (e) {
 			if (e.keyCode === 13 && this.$control_input.val() !== '') {
-				this.enterPressed = true;
+				this.__ds.enterPressed = true;
 			}
 
 			return original.apply(this, arguments);
@@ -41,7 +57,7 @@ Selectize.define('delayed_select_on_enter', function (options) {
 		var original = self.addItem;
 
 		return function (e) {
-			this.enterPressed = false;
+			this.__ds.enterPressed = false;
 
 			return original.apply(this, arguments);
 		};
